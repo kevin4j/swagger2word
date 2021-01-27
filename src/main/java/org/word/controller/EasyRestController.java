d03d4fdabf6b50e79921859c9121136c2072ccde
testdevops <379753498@qq.com>
2021-01-27 14:14:01 +0800

    增加导出excel自动下载功能 访问http://localhost:8080/downExcel?url={丝袜哥.json}

--- src/main/java/org/word/controller/EasyRestController.java
+++ src/main/java/org/word/controller/EasyRestController.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.word.service.WordService;
 import org.word.service.impl.SwaggerToExcelCase;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.Map;
+
 
 @Controller
 public class EasyRestController {
@@ -27,5 +23,12 @@ public class EasyRestController {
         swaggerToExcelCase.exec(filePath,url);
     }
 
-
+    @RequestMapping(value = "downExcel", method = RequestMethod.GET)
+    public void DownEasyUseCase(String url, HttpServletResponse response) {
+        try {
+            swaggerToExcelCase.downExcel(url,response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/word/service/impl/SwaggerToExcelCase.java
+++ src/main/java/org/word/service/impl/SwaggerToExcelCase.java
@@ -1,17 +1,22 @@
 package org.word.service.impl;
 
 import cn.hutool.core.date.DateTime;
+import com.alibaba.excel.EasyExcel;
 import com.testpro.easyrest.Util.ExcelUtil;
 import com.testpro.easyrest.Util.JsonUtil;
 import com.testpro.easyrest.bean.ExecutionData;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.word.model.Table;
 import org.word.service.WordService;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.*;
 
 @Slf4j
@@ -68,4 +73,32 @@ public class SwaggerToExcelCase {
 
     }
 
+    public void downExcel(String url, HttpServletResponse response) throws IOException {
+        Map<String, Object> stringObjectMap = tableService.tableList(url);
+
+        ArrayList<Table> tables = (ArrayList<Table>) stringObjectMap.get("tables");
+        ArrayList<ExecutionData> execList= new ArrayList<>();
+        for (Table table: tables){
+            ExecutionData  exec=new ExecutionData();
+            exec.setCaseName(table.getTag()+"_"+table.getDescription());
+            exec.setMethod(table.getRequestType().toUpperCase());
+            exec.setCaseDescription(table.getDescription());
+            exec.setUrl(table.getUrl()==null?"null":table.getUrl());
+            exec.setParameters(table.getRequestParam()==null?"":table.getRequestParam());
+            execList.add(exec);
+        }
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        //编码 防止中文乱码
+        String fileName = URLEncoder.encode("自动生成用例","UTF-8");
+        response.setHeader("Content-disposition","attachment;filename="+fileName+".xlsx");
+
+        //easyExcel.write第一个参数是输出流 第二个是依照哪个模板写出
+        //sheet是指设置工作簿名
+        //doWrite就是写出哪些数据
+        EasyExcel.write(response.getOutputStream(), ExecutionData.class)
+                .sheet("用户测试")
+                .doWrite(execList);
+
+    }
 }
